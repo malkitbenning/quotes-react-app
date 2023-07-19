8b2ca5226641bf27767bb9be267ab39580cbac0d
malkitbenning <malkitbenning@gmail.com>
2023-07-19 16:21:25 +0100

    button generating new quote working

--- src/MainQuoteArea.js
+++ src/MainQuoteArea.js
@@ -5,19 +5,26 @@ function MainQuoteArea() {
   const [mainQuoteText, setMainQuoteText] = useState("Loading, Please Wait...");
   const [mainQuoteAuthor, setMainQuoteAuthor] = useState("");
 
-  useEffect(() => {
+  function fetchRandomQuote() {
     fetch(`https://malkitbenning-quote-server.glitch.me/quotes/random/`)
       .then((res) => res.json())
       .then((data) => {
         setMainQuoteText(data.quote);
         setMainQuoteAuthor(data.author);
       });
+  }
+
+  useEffect(() => {
+    fetchRandomQuote();
   }, []);
 
   return (
     <div className="main-quote-area">
       <p className="quote-text">{mainQuoteText}</p>
       <p className="quote-author">{mainQuoteAuthor}</p>
+      <button type="button" className="quote-btn" onClick={fetchRandomQuote}>
+        Generate New Quote
+      </button>
     </div>
   );
 }
